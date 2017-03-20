a9c5d1ea2047d0a4183d055ac9939399291b6af2
nike-t <niketjadhav@gmail.com>
2017-03-20 03:03:59 -0700

    TTT Logic, to match Dan's format

--- JavaBoardgame/src/Games/TicTacToeGame.java
+++ JavaBoardgame/src/Games/TicTacToeGame.java
@@ -1,121 +1,74 @@
 package Games;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+
 import javax.swing.ImageIcon;
+
+import Server.ServerObject;
 
 /**
  * @author Daniel Ackerman 23104834
  * @version 0.1.0, 3/5/17
  */
-public class TicTacToeGame extends Game implements MouseListener{
+public class TicTacToeGame extends Game{
 
-    String currentPlayer="Player2";
-    String Player1="Player1";
-    String Player2="Player2";
-    Boolean redoMove=false;
+//    String currentPlayer;
+//    String Player1="Player1";
+//    String Player2="Player2";
+//    Boolean redoMove=false;
+	
+	TicTacToeLogic gameLogic;
 
     public TicTacToeGame(AbstractGameFactory tttgf)   {
         super("TicTacToe", tttgf);
-        for (Tile[] tt : board.boardMatrix) {
-            for(Tile t : tt)    {
-                t.addMouseListener(this);
-            }
-        }
+        gameLogic= new TicTacToeLogic(this, state);
+
+    }
+    
+    public void switchTurn(){
+    	super.switchTurn();
+        gameLogic.takeTurn(isTurn, state);
     }
 
-    public boolean checkWinner(){
-        return   ((board.boardMatrix[0][0].getOwned()!=null && board.boardMatrix[0][0].getOwned()==board.boardMatrix[0][1].getOwned() && board.boardMatrix[0][0].getOwned()==board.boardMatrix[0][2].getOwned())
-                ||(board.boardMatrix[1][0].getOwned()!=null && board.boardMatrix[1][0].getOwned()==board.boardMatrix[1][1].getOwned() && board.boardMatrix[1][0].getOwned()==board.boardMatrix[1][2].getOwned())
-                ||(board.boardMatrix[2][0].getOwned()!=null && board.boardMatrix[2][0].getOwned()==board.boardMatrix[2][1].getOwned() && board.boardMatrix[2][0].getOwned()==board.boardMatrix[2][2].getOwned())
-                ||(board.boardMatrix[0][0].getOwned()!=null && board.boardMatrix[0][0].getOwned()==board.boardMatrix[1][0].getOwned() && board.boardMatrix[0][0].getOwned()==board.boardMatrix[2][0].getOwned())
-                ||(board.boardMatrix[0][1].getOwned()!=null && board.boardMatrix[0][1].getOwned()==board.boardMatrix[1][1].getOwned() && board.boardMatrix[0][1].getOwned()==board.boardMatrix[2][1].getOwned())
-                ||(board.boardMatrix[0][2].getOwned()!=null && board.boardMatrix[0][2].getOwned()==board.boardMatrix[1][2].getOwned() && board.boardMatrix[0][2].getOwned()==board.boardMatrix[2][2].getOwned())
-                ||(board.boardMatrix[0][0].getOwned()!=null && board.boardMatrix[0][0].getOwned()==board.boardMatrix[1][1].getOwned() && board.boardMatrix[0][0].getOwned()==board.boardMatrix[2][2].getOwned())
-                ||(board.boardMatrix[0][2].getOwned()!=null && board.boardMatrix[0][2].getOwned()==board.boardMatrix[1][1].getOwned() && board.boardMatrix[0][2].getOwned()==board.boardMatrix[2][0].getOwned())
-        );
-    }
-
-    public boolean boardFilled(){
-        for (Tile[] tt : board.boardMatrix) {
-            for (Tile t : tt)   {
-                if (t.getOwned()==null) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    public boolean canMakeMove(int row, int column, String player){
-        return (player==currentPlayer && board.boardMatrix[row][column].getOwned()==null );
-    }
-
-    public String flipTurn(String currentPlayer){
-        if (currentPlayer==Player2 && !redoMove){
-            currentPlayer=Player1;
-        }
-        else if (currentPlayer==Player1 && !redoMove){
-            currentPlayer=Player2;
-        }
-        return currentPlayer;
-    }
+  
 
     protected void run(Tile clicked) {
-        ImageIcon piece;
-        currentPlayer=flipTurn(currentPlayer);
-        if (currentPlayer==Player1){piece=client.playerPieces.get(0);}
-        else{piece=opponent.playerPieces.get(1);}
-        if (canMakeMove(clicked.getRow(),clicked.getColumn(), currentPlayer)){
-            board.boardMatrix[clicked.getRow()][clicked.getColumn()].setOwned(currentPlayer);
-            clicked.addPiece(piece);
-            System.out.println("Move made by "+ currentPlayer);
-            redoMove=false;
-            if (checkWinner()){
-                System.out.println("Winner is "+ currentPlayer);
-                System.exit(0);
-            }
-            if (boardFilled()){
-                System.out.println("Board Full! It's a tie");
-                System.exit(0);
-            }
-        }else{
-            System.out.println("Can't make move");
-            redoMove=true;
-        }
-    }
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        Tile clicked = (Tile)e.getComponent();
-        run(clicked);
-    }
-
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-
+//        ImageIcon piece;
+//        currentPlayer=flipTurn(currentPlayer);
+//        if (currentPlayer==Player1){piece=client.playerPieces.get(0);}
+//        else{piece=opponent.playerPieces.get(1);}
+//        if (canMakeMove(clicked.getRow(),clicked.getColumn(), currentPlayer)){
+//            board.boardMatrix[clicked.getRow()][clicked.getColumn()].setOwned(currentPlayer);
+//            clicked.addPiece(piece);
+//            System.out.println("Move made by "+ currentPlayer);
+//            redoMove=false;
+//            if (checkWinner()){
+//                System.out.println("Winner is "+ currentPlayer);
+//                System.exit(0);
+//            }
+//            if (boardFilled()){
+//                System.out.println("Board Full! It's a tie");
+//                System.exit(0);
+//            }
+//        }else{
+//            System.out.println("Can't make move");
+//            redoMove=true;
+//        }
     }
 
 
     @Override
     public void runGame() {
-
+    	if (!state.gameOver) {
+            GameState returnedState = gameLogic.takeTurn(isTurn, state);
+        }
     }
+
+//	@Override
+//	public void serverMakeMove(ServerObject s) {
+//		//currentPlayer=s.getSender();
+//		
+//	}
 
 	@Override
 	public GameState doMove() {
---
+++ JavaBoardgame/src/Games/TicTacToeLogic.java
@@ -0,0 +1,114 @@
+package Games;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class TicTacToeLogic implements MouseListener {
+	
+	TicTacToeGame ttt;
+    GameState state;
+    Player isTurn;
+    Tile[][] tileBoard;
+    boolean redoMove=true;
+	public TicTacToeLogic(TicTacToeGame game, GameState state) {
+		this.ttt=game;
+		this.state=state;
+		this.isTurn=game.isTurn;
+		this.tileBoard=game.board.boardMatrix;
+		for(Tile[] tt : tileBoard)   {
+            for(Tile t : tt)    {
+                t.addMouseListener(this);
+            }
+        }
+	}
+	
+    public boolean checkWinner(){
+        return   ((tileBoard[0][0].getOwner()!=null && tileBoard[0][0].getOwner()==tileBoard[0][1].getOwner() && tileBoard[0][0].getOwner()==tileBoard[0][2].getOwner())
+                ||(tileBoard[1][0].getOwner()!=null && tileBoard[1][0].getOwner()==tileBoard[1][1].getOwner() && tileBoard[1][0].getOwner()==tileBoard[1][2].getOwner())
+                ||(tileBoard[2][0].getOwner()!=null && tileBoard[2][0].getOwner()==tileBoard[2][1].getOwner() && tileBoard[2][0].getOwner()==tileBoard[2][2].getOwner())
+                ||(tileBoard[0][0].getOwner()!=null && tileBoard[0][0].getOwner()==tileBoard[1][0].getOwner() && tileBoard[0][0].getOwner()==tileBoard[2][0].getOwner())
+                ||(tileBoard[0][1].getOwner()!=null && tileBoard[0][1].getOwner()==tileBoard[1][1].getOwner() && tileBoard[0][1].getOwner()==tileBoard[2][1].getOwner())
+                ||(tileBoard[0][2].getOwner()!=null && tileBoard[0][2].getOwner()==tileBoard[1][2].getOwner() && tileBoard[0][2].getOwner()==tileBoard[2][2].getOwner())
+                ||(tileBoard[0][0].getOwner()!=null && tileBoard[0][0].getOwner()==tileBoard[1][1].getOwner() && tileBoard[0][0].getOwner()==tileBoard[2][2].getOwner())
+                ||(tileBoard[0][2].getOwner()!=null && tileBoard[0][2].getOwner()==tileBoard[1][1].getOwner() && tileBoard[0][2].getOwner()==tileBoard[2][0].getOwner())
+        );
+    }
+
+    public boolean boardFilled(){
+        for (Tile[] tt : tileBoard) {
+            for (Tile t : tt)   {
+                if (t.getOwner()==null) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    
+	public GameState takeTurn(Player playerTurn, GameState state) {
+        this.isTurn = playerTurn;
+        this.state = new GameState(state);
+        redoMove = true;
+        state.gameOver=(checkWinner() || boardFilled());
+		return state;		
+	}
+	
+    public boolean canMakeMove(int row, int column){
+        return (tileBoard[row][column].getOwner()==null );
+    }
+    
+    public void makeMove(int row, int column, Tile clicked){
+    	tileBoard[row][column].setOwner(isTurn);
+        clicked.addPiece(isTurn.playerPieces.get(0));
+        System.out.println("Move made by "+ isTurn.name);
+        redoMove=false;
+    }
+    
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		Tile clicked = (Tile)e.getComponent();
+		if (redoMove){
+			if (canMakeMove(clicked.getRow(), clicked.getColumn())){
+				makeMove(clicked.getRow(), clicked.getColumn(), clicked);
+		        if (checkWinner()){
+		        	System.out.println("Winner is "+ isTurn.name);
+		        }
+		        if (boardFilled()){
+		        	System.out.println("Board Full! It's a tie");
+		        }
+				ttt.switchTurn();
+			}else{
+	            System.out.println("Can't make move");
+	            redoMove=true;
+			}
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
