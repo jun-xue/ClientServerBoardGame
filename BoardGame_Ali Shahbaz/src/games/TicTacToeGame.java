b32d7fb485db42106b7ae31c3fb866b5ac638cb4
Zachary Stuart <stuartz@uci.edu>
2017-03-20 19:27:19 -0700

    Delete TicTacToeGame.java

--- BoardGame_Ali Shahbaz/src/games/TicTacToeGame.java
+++
@@ -1,9 +0,0 @@
-package games;
-
-public class TicTacToeGame extends Game {
-
-	public TicTacToeGame(String gameTitle, int boardWidth, int boardHeight, int maxPlayers, int minPlayers,
-			GameLogic gameLogic) {
-		super(gameTitle, boardWidth, boardHeight, maxPlayers, minPlayers, gameLogic);
-	}
-}
